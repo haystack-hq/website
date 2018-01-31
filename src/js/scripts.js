bafd0dec1b57e41ea40a92d522684daf09f9b2bf
Dan Dan Ahrendt <daniel@gosolid.net>
2018-01-30 18:36:39 -0800

    Attempt to focus on email field on touch, removed debug code

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3053,18 +3053,6 @@ mr = (function (mr, $, window, document){
                 }
             }
         });
-
-        $('#contact-us .contact-email').on('click touch', function(e){
-
-            if(jQuery('html').hasClass('device-ios')){
-                console.log('1');
-                e.preventDefault();
-                console.log($('#contact-us .contact-email'));
-                setTimeout(function(){
-                    $('#contact-us .contact-email').focus();
-                }, 500);
-            }
-        });
     };
 
     mr.components.documentReady.push(mr.clicktouch.documentReady);
