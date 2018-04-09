2a635f34f9e9ba8f13c36d4c4e6e664673e3987e
Dan Dan Ahrendt <daniel@gosolid.net>
2018-04-09 12:28:55 -0700

    Turned Contact Info card into an includes component

--- src/components/EarlyAccess.vue
+++ src/components/EarlyAccess.vue
@@ -37,30 +37,7 @@
                                 </div>
                             </form>
                         </div>
-                        <div class="col-md-5">
-                            <div class="ui-card form-card">
-                                <div class="card-header pb-0">
-                                    <h3 class="heading">Contact Info</h3>
-                                </div>
-                                <div class="card-body">
-                                    <ul class="ui-icon-blocks ui-blocks-v">
-                                        <li class="ui-icon-block">
-                                            <span class="icon icon-clock"></span>
-                                            <p>Mon - Fri 09:00am - 5:00pm</p>
-                                        </li>
-                                        <li class="ui-icon-block">
-                                            <span class="icon icon-envelope"></span>
-                                            <p>support@haystackhq.com</p>
-                                        </li>
-                                        <li class="ui-icon-block">
-                                            <span class="icon icon-pin"></span>
-                                            <p>107 Spring St. <br>
-                                                Seattle, WA 98104</p>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
+                        <contact-info/>
                     </div>
                 </div><!-- .container -->
             </div><!-- .section -->
@@ -70,10 +47,11 @@
 
 <script>
 import LayoutBase from '@/components/layouts/Base'
+import ContactInfo from '@/components/includes/ContactInfo'
 
 export default {
   name: 'Home',
-  components: {LayoutBase},
+  components: {LayoutBase, ContactInfo},
   data () {
     return {
 
---
+++ src/components/includes/ContactInfo.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="col-md-5">
+        <div class="ui-card form-card">
+            <div class="card-header pb-0">
+                <h3 class="heading">Contact Info</h3>
+            </div>
+            <div class="card-body">
+                <ul class="ui-icon-blocks ui-blocks-v">
+                    <li class="ui-icon-block">
+                        <span class="icon icon-clock"></span>
+                        <p>Mon - Fri 9AM to 5PM PT</p>
+                    </li>
+                    <li class="ui-icon-block">
+                        <span class="icon icon-envelope"></span>
+                        <p>support@haystackhq.com</p>
+                    </li>
+                    <li class="ui-icon-block">
+                        <span class="icon icon-pin"></span>
+                        <p>107 Spring St. <br>
+                            Seattle, WA 98104</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'ContactInfo',
+  data () {
+    return {}
+  }
+}
+</script>
