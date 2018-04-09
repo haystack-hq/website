10950b4d1d5a86283c6e0698bc8ca8a51ce94146
Jaime Lopez <jaime@gosolid.net>
2018-04-09 16:35:19 -0700

    removed commented and not used features on base test

--- test/unit/specs/Base.spec.js
+++ test/unit/specs/Base.spec.js
@@ -3,15 +3,12 @@ import router from '@/routes'
 import { shallow } from '@vue/test-utils'
 import Base from '@/components/layouts/Base'
 import http from 'http'
-//import sinon from 'sinon'
 
 describe('Base', () => {
   let wrapper
 
   beforeEach(() => {
-    wrapper = shallow(Base, {
-        router
-    })
+    wrapper = shallow(Base)
   })
 
   it('should display menu item links', () => {
@@ -26,62 +23,4 @@ describe('Base', () => {
     // button
     Vue.see(wrapper, '<a href="/early-access" class="btn early-access btn-sm pull-right">Get Early Access</a>')
   })
-
-  //it('should redirect to the homepage when the logo is clicked', (done) => {
-  //  Vue.click(wrapper, '.ui-variable-logo.navbar-brand')
-  //
-  //  Vue.nextTick(() => {
-  //    expect(wrapper.vm.$route.path).toBe('/')
-  //
-  //    done()
-  //  })
-  //})
-  //
-  //it('should redirect to the blog when the blog link is clicked', (done) => {
-  //  var spy = sinon.spy(wrapper.vm.$router, 'push')
-  //
-  //  Vue.click(wrapper, '.menu-link.blog a')
-  //
-  //  Vue.nextTick(() => {
-  //    expect(spy.calledWith('/blog')).toBe(true)
-  //
-  //    done()
-  //
-  //    wrapper.vm.$router.push.restore()
-  //  })
-  //})
-  //
-  //it('should redirect to the contact-us page when the contact us link is clicked', (done) => {
-  //  Vue.click(wrapper, '.menu-link.contact a')
-  //
-  //  Vue.nextTick(() => {
-  //    expect(wrapper.vm.$route.path).toBe('/contact-us')
-  //
-  //    done()
-  //  })
-  //})
-  //
-  //it('should redirect to github when the github link is clicked', (done) => {
-  //  var spy = sinon.spy(wrapper.vm.$router, 'push')
-  //
-  //  Vue.click(wrapper, '.menu-link.git a')
-  //
-  //  Vue.nextTick(() => {
-  //    expect(spy.calledWith('/github')).toBe(true)
-  //
-  //    done()
-  //
-  //    wrapper.vm.$router.push.restore()
-  //  })
-  //})
-  //
-  //it('should redirect to the early-access page when the get early access link is clicked', (done) => {
-  //  Vue.click(wrapper, '.btn.early-access')
-  //
-  //  Vue.nextTick(() => {
-  //    expect(wrapper.vm.$route.path).toBe('/early-access')
-  //
-  //    done()
-  //  })
-  //})
 })
